34d5600b46ae1405cb9d72f4059ee6609a33dd17
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-01-20 19:04:40 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#file name = 'D:\\sales_data.xlsx'
+file_name = "C:\excel\data_py.xlsx"
+
+# Step 1: Load your Excel data
+try:
+    data = pd.read_excel(file_name)
+
+    print(data.head())
+except FileNotFoundError:
+    print("{} not found".format(file_name))
