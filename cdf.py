52066d4b6f94de4453458008f8d3c79d1c8c03e2
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-01-21 19:46:49 +0530

    To view the results at terminal

--- cdf.py
+++ cdf.py
@@ -8,13 +8,13 @@ csv_path = r"C:\Users\subha\Documents\data.csv"
 # From dictionary
 data = {
     'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35],
+    'Age': [25, 31, 35],
     'Salary': [50000, 60000, 70000]
 }
 df = pd.DataFrame(data)
 
-# df.to_excel(file_name, index=False)
-# df.to_csv(csv_path)
+df.to_excel(file_name, index=False)
+df.to_csv(csv_path)
 
 # From CSV
 df = pd.read_csv(csv_path)
